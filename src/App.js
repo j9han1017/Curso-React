44bc67a9cb721ede014dd85ab311849cd2564d94
j9han1017 <johandiaz101706@gmail.com>
2024-05-08 18:42:55 -0500

    Props completado

--- src/App.js
+++ src/App.js
@@ -2,24 +2,36 @@ import { TodoCounter } from './TodoCounter';
 import { TodoSearch } from './TodoSearch';
 import { TodoList } from './TodoList';
 import { TodoItem } from './TodoItem';
-import { CreateTodoButton} from './CreateTodoButton'
+import { CreateTodoButton } from './CreateTodoButton'
 import './App.css';
+import React from 'react'
 
+const defaultTodos = [
+  { text: 'Cortar cebollla', completed: true },
+  { text: 'Tomar curso de Intro a React.js', completed: false },
+  { text: 'Llora con la llorona', completed: false },
+  { text: 'Ir al gym', completed: true },
+  { text: 'Ir al gym', completed: true },
+]
 function App() {
   return (
-    <div className="App">
+    <React.Fragment>
 
       <TodoCounter completed={16} total={25} />
       <TodoSearch />
 
       <TodoList>
-        <TodoItem />
-        <TodoItem />
-        <TodoItem />
+        {defaultTodos.map(todo => (
+          <TodoItem
+            key={todo.text}
+            text={todo.text}
+            completed={todo.completed}
+          />
+        ))}
       </TodoList>
-      
+
       <CreateTodoButton />
-    </div>
+    </React.Fragment>
   );
 }
 export default App;
